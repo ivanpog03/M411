831d3544b5c1eda0d2e7d0b34c9ef234600150d8
pogadaevi <pogadaevi@bzz.ch>
2022-01-26 14:21:05 +0100

    KUI finished

--- Projektarbeit/src/KUI.java
+++ Projektarbeit/src/KUI.java
@@ -6,13 +6,22 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 
+/**
+ *In KUI werden die Testdaten in arrays gespeichert, sortiert und schliesslich in eine neu-generierte Excel-Datei gespeichert.
+ * @author Ivan Häring
+ */
+
 public class KUI{
 
+    /**
+     * Hier wird ein neues Workbook erstellt und die Seiten, welche nachher in der Excel-Datei gespeichert werden.
+     * Ein externes Bibliothek wird benutzt-->poi-bin-5.2.0-20220106-->lib, ooxml-lib und alle Daten unter dem ooxml-lib Ordner
+     * in INTELLIJ Project Structure importieren lassen (libraries-->+-->Java) Screenshot von Daten im Ordner verfügbar
+     */
     public static XSSFWorkbook workbook=new XSSFWorkbook();
     public static XSSFSheet sheetQuicksortFirstPivot=workbook.createSheet("QuickSortFirstPivot");
     public static XSSFSheet sheetHeapsort= workbook.createSheet("HeapSort");
@@ -23,8 +32,12 @@ public class KUI{
     public static XSSFSheet sheetBST= workbook.createSheet("BST");
 
 
-
-
+    /**
+     *Array mit Testdaten wird mit QuickSortFirstPivot sortiert. Danach werden die Ergebnisse im sheet gespeichert.
+     * @param array Array mit Testdaten
+     * @param i wird gebraucht, um Daten in der Tabelle am richtigen Ort zu speichern
+     * @param input wird gebraucht, um den Namen von der Testdatei einzugeben
+     */
     public void quicksortFirstPivot (int[] array, int i, String input){
         Sortieralgorithmus quicksortFirstPivot = new QuicksortFirstPivot();
         quicksortFirstPivot.sort(array);
@@ -37,6 +50,12 @@ public class KUI{
 
     }
 
+    /**
+     * Array mit Testdaten wird mit HeapSort sortiert. Danach werden die Ergebnisse im sheet gespeichert.
+     * @param array Array mit Testdaten
+     * @param i wird gebraucht, um Daten in der Tabelle am richtigen Ort zu speichern
+     * @param input wird gebraucht, um den Namen von der Testdatei einzugeben
+     */
     public void heapSort (int[] array, int i, String input){
         Sortieralgorithmus heapsort=new Heapsort();
         heapsort.sort(array);
@@ -47,9 +66,14 @@ public class KUI{
         sheetHeapsort.getRow(3).createCell(i).setCellValue(heapsort.getSpeicherbedarf());
         sheetHeapsort.getRow(4).createCell(i).setCellValue(heapsort.getSchreibzugriffe());
 
-
     }
 
+    /**
+     * Array mit Testdaten wird mit InsertionSort sortiert. Danach werden die Ergebnisse im sheet gespeichert.
+     * @param array Array mit Testdaten
+     * @param i wird gebraucht, um Daten in der Tabelle am richtigen Ort zu speichern
+     * @param input wird gebraucht, um den Namen von der Testdatei einzugeben
+     */
     public void insertionSort (int[] array, int i, String input){
         Insertionsort insertionsort=new Insertionsort();
         insertionsort.sort(array);
@@ -60,9 +84,14 @@ public class KUI{
         sheetInsertionSort.getRow(3).createCell(i).setCellValue(insertionsort.getSpeicherbedarf());
         sheetInsertionSort.getRow(4).createCell(i).setCellValue(insertionsort.getSchreibzugriffe());
 
-
     }
 
+    /**
+     * Array mit Testdaten wird mit MergeSort sortiert. Danach werden die Ergebnisse im sheet gespeichert.
+     * @param array Array mit Testdaten
+     * @param i wird gebraucht, um Daten in der Tabelle am richtigen Ort zu speichern
+     * @param input wird gebraucht, um den Namen von der Testdatei einzugeben
+     */
     public void mergeSort (int[] array, int i, String input){
         Sortieralgorithmus mergesort=new Mergesort();
         mergesort.sort(array);
@@ -75,6 +104,12 @@ public class KUI{
 
     }
 
+    /**
+     * Array mit Testdaten wird mit SelectionSort sortiert. Danach werden die Ergebnisse im sheet gespeichert.
+     * @param array Array mit Testdaten
+     * @param i wird gebraucht, um Daten in der Tabelle am richtigen Ort zu speichern
+     * @param input wird gebraucht, um den Namen von der Testdatei einzugeben
+     */
     public void selectionSort (int[] array, int i, String input){
         Sortieralgorithmus selectionsort=new Selectionsort();
         selectionsort.sort(array);
@@ -87,6 +122,12 @@ public class KUI{
 
     }
 
+    /**
+     * Array mit Testdaten wird mit QuickSortRandomPivot sortiert. Danach werden die Ergebnisse im sheet gespeichert.
+     * @param array Array mit Testdaten
+     * @param i wird gebraucht, um Daten in der Tabelle am richtigen Ort zu speichern
+     * @param input wird gebraucht, um den Namen von der Testdatei einzugeben
+     */
     public void quicksortRandomPivot (int[] array, int i, String input){
         Sortieralgorithmus quicksortrandompivot=new QuicksortRandomPivot();
         quicksortrandompivot.sort(array);
@@ -100,6 +141,12 @@ public class KUI{
 
     }
 
+    /**
+     * Array mit Testdaten wird mit BST sortiert. Danach werden die Ergebnisse im sheet gespeichert.
+     * @param array Array mit Testdaten
+     * @param i wird gebraucht, um Daten in der Tabelle am richtigen Ort zu speichern
+     * @param input wird gebraucht, um den Namen von der Testdatei einzugeben
+     */
     public void bST (int[] array, int i, String input){
         Sortieralgorithmus bst=new BST(0);
         bst.sort(array);
@@ -113,15 +160,22 @@ public class KUI{
 
     }
 
+    /**
+     * Zeilen werden in den sheets erstellt und die Methoden werden mit den Arrays aufgerufen. Am Schluss wird alles in eine Excel-Datei gespeichert
+     * Es kommt möglicherweise ein ERROR beim Ausführen, Programm wird aber weiterhin laufen und alles in Excel-Datei speichern. Wenn es fertig
+     * ist kommt eine Meldung "finished".
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
-
+/**
+ * Zeilen werden erstellt
+ */
         sheetQuicksortFirstPivot.createRow(0);
         sheetQuicksortFirstPivot.createRow(1);
         sheetQuicksortFirstPivot.createRow(2);
         sheetQuicksortFirstPivot.createRow(3);
         sheetQuicksortFirstPivot.createRow(4);
-
-
 
         sheetHeapsort.createRow(0);
         sheetHeapsort.createRow(1);
@@ -129,13 +183,11 @@ public class KUI{
         sheetHeapsort.createRow(3);
         sheetHeapsort.createRow(4);
 
-
         sheetInsertionSort.createRow(0);
         sheetInsertionSort.createRow(1);
         sheetInsertionSort.createRow(2);
         sheetInsertionSort.createRow(3);
         sheetInsertionSort.createRow(4);
-
 
         sheetMergeSort.createRow(0);
         sheetMergeSort.createRow(1);
@@ -143,13 +195,11 @@ public class KUI{
         sheetMergeSort.createRow(3);
         sheetMergeSort.createRow(4);
 
-
         sheetSelectionSort.createRow(0);
         sheetSelectionSort.createRow(1);
         sheetSelectionSort.createRow(2);
         sheetSelectionSort.createRow(3);
         sheetSelectionSort.createRow(4);
-
 
         sheetQicksortRandomPivot.createRow(0);
         sheetQicksortRandomPivot.createRow(1);
@@ -157,42 +207,33 @@ public class KUI{
         sheetQicksortRandomPivot.createRow(3);
         sheetQicksortRandomPivot.createRow(4);
 
-
         sheetBST.createRow(0);
         sheetBST.createRow(1);
         sheetBST.createRow(2);
         sheetBST.createRow(3);
         sheetBST.createRow(4);
-
-
+/**
+ * KUI-Objekt wird erstellt
+ */
         KUI kui=new KUI();
 
-
-
+        /**
+         * Daten werden in Arrays gespeichert
+         */
         int[] inversTeilSortiert1000= readFiles("Testdaten\\InversTeilsortiert1000.dat");
-       // System.out.println(Arrays.toString(inversTeilSortiert1000));
         int[] inversTeilSortiert10000= readFiles("Testdaten\\InversTeilsortiert10000.dat");
-        //System.out.println(Arrays.toString(inversTeilSortiert10000));
         int[] inversTeilSortiert100000= readFiles("Testdaten\\InversTeilsortiert100000.dat");
-       // System.out.println(Arrays.toString(inversTeilSortiert100000));
-
         int[] random1000= readFiles("Testdaten\\Random1000.dat");
-       // System.out.println(Arrays.toString(random1000));
         int[] random10000= readFiles("Testdaten\\Random10000.dat");
-       // System.out.println(Arrays.toString(random10000));
         int[] random100000= readFiles("Testdaten\\Random100000.dat");
-       // System.out.println(Arrays.toString(random100000));
-
         int[] teilsortiert1000= readFiles("Testdaten\\Teilsortiert1000.dat");
-       // System.out.println(Arrays.toString(teilsortiert1000));
         int[] teilsortiert10000= readFiles("Testdaten\\Teilsortiert10000.dat");
-       // System.out.println(Arrays.toString(teilsortiert10000));
         int[] teilsortiert100000= readFiles("Testdaten\\Teilsortiert100000.dat");
-        //System.out.println(Arrays.toString(teilsortiert100000))
 
-        //Sortieren
-
-       kui.quicksortFirstPivot(inversTeilSortiert1000, 0,"inversTeilSortiert1000");
+        /**
+         * Methoden werden aufgerufen
+         */
+        kui.quicksortFirstPivot(inversTeilSortiert1000, 0,"inversTeilSortiert1000");
         kui.quicksortFirstPivot(inversTeilSortiert10000, 1,"inversTeilSortiert10000");
         kui.quicksortFirstPivot(inversTeilSortiert100000, 2,"inversTeilSortiert100000");
         kui.quicksortFirstPivot(random1000, 3,"random1000");
@@ -252,7 +293,6 @@ public class KUI{
         kui.quicksortRandomPivot(teilsortiert10000,7,"teilsortiert10000");
         kui.quicksortRandomPivot(teilsortiert100000,8,"teilsortiert100000");
 
-
         kui.bST(inversTeilSortiert1000,0,"inversTeilSortiert1000");
         kui.bST(inversTeilSortiert10000,1,"inversTeilSortiert10000");
         kui.bST(inversTeilSortiert100000,2,"inversTeilSortiert100000");
@@ -263,16 +303,23 @@ public class KUI{
         kui.bST(teilsortiert10000,7,"teilsortiert10000");
         kui.bST(teilsortiert100000,8,"teilsortiert100000z");
 
-
-
         System.out.println("finished");
 
+        /**
+         * Alles wird in lb2.xlsx gespeichert
+         */
         FileOutputStream out = new FileOutputStream(new File("lb2.xlsx"));
         workbook.write(out);
         out.close();
 
     }
 
+    /**
+     * Textdateien werden gelesen und im Array gespeichert
+     * @param file Textdatei (und Pfad)
+     * @return Array mit den Testdaten
+     * @throws FileNotFoundException
+     */
     private static int[] readFiles(String file) throws FileNotFoundException {
 
         File f=new File(file);
